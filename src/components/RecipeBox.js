b5b268bfd8118f949c5d5a5cd853cb764c284b86
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2016-09-11 10:40:02 +0200

    handle recipe list hover

--- src/components/RecipeBox.js
+++ src/components/RecipeBox.js
@@ -4,14 +4,26 @@ import MoreIcon from '../images/moreIcon.png';
 import StarIcon from '../images/starIcon.png';
 
 class RecipeBox extends Component {
+    componentWillMount() {
+      this.setState({isHovering: false});
+    }  
     starRecipeHandler(event) {
       console.log('Starring ' + this.props.recipeName, event);
     }
+    hoverHandler(){
+      this.setState({isHovering: !this.state.isHovering})
+    }
     render() {
       const name = 'recipe-box-' + this.props.recipeId;
+      const hoverClass = (this.state.isHovering)? 'recipeBox recipe-hover' : 'recipeBox recipe-no-hover';
+      
       return (
-        <div className="recipeBox">
+        <div className={hoverClass} 
+          onMouseOver={this.hoverHandler.bind(this)} 
+          onMouseOut={this.hoverHandler.bind(this)}>
+          
 				  <img src={this.props.recipeImage}></img>
+          
           <div className="recipeDesc">
             <img id={name} className="more-icon" src={MoreIcon}></img>
             <h3 className="recipe-box-text">{this.props.recipeName}</h3>
